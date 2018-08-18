c01833bbe7f0cbf3c9a9a29858b69e95049cdf50
kinglyjn <admin@keyllo.com>
2018-08-19 02:45:09 +0800

    update file: test01 App.java, application.yml

--- src/main/java/test01/hello/App.java
+++ src/main/java/test01/hello/App.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * 测试 @SpringBootApplication 注解
  * 使用@SpringbootApplication注解  可以解决根类或者配置类（我自己的说法，就是main所在类）头上注解过多的问题，
- * 一个@SpringbootApplication相当于@Configuration,@EnableAutoConfiguration和 @ComponentScan 并具有
+ * 一个@SpringbootApplication相当于@Configuration,@EnableAutoConfiguration和@ComponentScan 并具有
  * 他们的默认属性值。
+ * 
+ * [注意] App.java文件不能直接放在main/java文件夹下，必须要建一个包把他放进去
+ *
  *
  */
 @SpringBootApplication
